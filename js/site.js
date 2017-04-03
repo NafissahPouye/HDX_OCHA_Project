52fbdd55d89b0db14996665cfa77b9844bd76b6f
Nafissah Pouye <Nafissah Pouye>
2017-04-03 15:44:09 +0000

    First Commit

--- js/site.js
+++ js/site.js
@@ -1,66 +1,42 @@
-
-
-function generateringComponent(vardata, vargeodata){
+function generatingComponent(vardata, vargeodata){
 
   var lookUp = genLookup(vargeodata) ;
-
-
-  var chPhase = dc.rowChart('#phase') ;
-  var chTrend = dc.rowChart('#trend') ;
+  var trends = dc.compositeChart('#CompositeChart') ;
   var chCarte = dc.leafletChoroplethChart('#carte') ;
-
   var cf = crossfilter(vardata);
   var all = cf.groupAll();
+  var chCarteDim = cf.dimension(function (d) { return d.country}) ;
+  var chCarteGroup = chCarteDim.group().reduceSum( function (d) { return d.phase1+d.phase2+d.phase3to5+d.project_requirement}) ;
+  var dateDimension = cf.dimension(function (d) { return d.date});
+  var groupPhase1 = dateDimension.group().reduceSum(function (d){ return d.phase1});
+  var groupPhase2 = dateDimension.group().reduceSum(function (d){ return d.phase2});
+  var groupPhase3to5 = dateDimension.group().reduceSum(function (d){ return d.phase3to5});
+  var groupRequirements = dateDimension.group().reduceSum(function (d){ return d.project_requirement});
 
-  // graphe des phases
-    var chPhaseDim = cf.dimension(function(d){ return d.country}) ;
-    var chPhaseGroupe = chPhaseDim.group().reduceSum( function (d) { return d.phase1}) ;
-
-  //graphe trend
-
-    var chTrendDim = cf.dimension(function (d) { return d.rowcacode2}) ;
-    var chTrendGroupe = chTrendDim.group().reduceSum( function (d) { return d.phase2}) ;
-
-  // carte
-
-    var chCarteDim = cf.dimension(function (d) { return d.rowcacode2}) ;
-    var chCarteGroupe = chCarteDim.group().reduceSum( function (d) { return d.phase1}) ;
-
-    // function graphePerLocation(code) {
-    //   chCarteDim.filter(code);
-    //
-    //   var ndx = crossfilter(chCarteDim.top(Infinity));
-    //
-    //   var dimLoc = ndx.dimension(function(d){return  d.phase1});
-    //   var groupeLoc = dimLoc.group().reduceSum(function(d) { return d.value});
-    //
-    //   chTrend.dimension(dimLoc)
-    //         .group(groupeLoc);
-    //   return null;
-    // }
-
-    chPhase.width(450)
-              .height(230)
-              .dimension(chPhaseDim)
-              .group(chPhaseGroupe) ;
-
-    chTrend.width(450)
-           .height(230)
-           .dimension(chTrendDim)
-           .group(chTrendGroupe)
-           .data(function (group) {
-              return group.top(5) ;
-            });
-           //.x(d3.scale.linear().domain([1,9]));
-
+ trends
+      .width(550)
+      .height(475)
+      .dimension(dateDimension)
+      .x(d3.scale.linear().domain([2014, 2016]))
+      .elasticY(true)
+      .compose([
+        dc.lineChart(trends).group(groupPhase1, 'Phase 1'),
+        dc.lineChart(trends).group(groupPhase2, 'Phase 2'),
+        dc.lineChart(trends).group(groupPhase3to5, 'Phase 3 to 5'),
+        dc.lineChart(trends).group(groupRequirements, 'Project Requirement')
+        ])
+      .brushOn(false)
+      //.xAxis();
+    
   dc.dataCount('count-info')
     .dimension(cf)
     .group(all);
 
+//define the map
 
       chCarte.width(450)
              .dimension(chCarteDim)
-             .group(chCarteGroupe)
+             .group(chCarteGroup)
              .center([0,0])
              .zoom(0)
              .geojson(vargeodata)
@@ -74,21 +50,12 @@ function generateringComponent(vardata, vargeodata){
                }
              })
              .featureKeyAccessor(function (feature){
-               return feature.properties['Rowcacode2'];
+               return feature.properties['ISO3'];
              }).popup(function (d){
                return lookUp[d];
              })
              .renderPopup(true);
-            //  .on("filtered", function (chart) {
-            //    dc.events.trigger(function () {
-            //      if(chart.filter()) {
-            //        console.log('valeur filtree : '+chart.filter());
-            //        var cdm = chart.filter();
-            //        graphePerLocation(cdm);
-            //      }
-            //    });
-            //  });
-
+           
       dc.renderAll();
 
       var map = chCarte.map();
@@ -103,23 +70,21 @@ function generateringComponent(vardata, vargeodata){
       function genLookup(geojson) {
         var lookup = {} ;
         geojson.features.forEach(function (e) {
-          lookup[e.properties['Rowcacode2']] = String(e.properties['adm2']);
+          lookup[e.properties['ISO3']] = String(e.properties['NAME']);
         });
         return lookup ;
       }
-
-
 }
 
 var dataCall = $.ajax({
     type: 'GET',
-    url: 'data/ch.json',
+    url: 'data/datas.json',
     dataType: 'json',
 });
 
 var geomCall = $.ajax({
     type: 'GET',
-    url: 'data/sahel.geojson',
+    url: 'data/wa.geojson',
     dataType: 'json',
 });
 
@@ -127,7 +92,7 @@ var geomCall = $.ajax({
 $.when(dataCall, geomCall).then(function(dataArgs, geomArgs){
     var geom = geomArgs[0];
     geom.features.forEach(function(e){
-        e.properties['Rowcacode2'] = String(e.properties['Rowcacode2']);
+        e.properties['ISO3'] = String(e.properties['NAME']);
     });
-    generateringComponent(dataArgs[0],geom);
+    generatingComponent(dataArgs[0],geom);
 });
